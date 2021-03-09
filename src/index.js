4a2ff9e9033bc8af73c914a58ed417657b91427b
sten00777 <www.ep965@gmail.com>
2021-03-09 19:09:58 +0300

    refactor: modified index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length !== 0){
+    return Math.min.apply(null, array);
+  }else {
+    return 0;
+  }
 }
+  
+ 
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length !== 0){
+    return Math.max.apply(null, array);
+  }else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length !== 0){
+    let res = 0;
+    for (let i = 0; i < array.length; i++){
+     res += array[i];
+    }
+    return res / array.length;
+  }else {
+    return 0;
+  }
 }
